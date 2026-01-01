216a2701998b19dd5a1ead47ecb9597f88d7a822
DevMartinsF <r.martins.programmer@gmail.com>
2026-01-01 17:17:45 -0300

    feat: add constructor with auto-generated id

--- src/com/bank/model/Client.java
+++ src/com/bank/model/Client.java
@@ -7,6 +7,9 @@ public class Client {
     private String name;
     private String email;
 
-
-
+    public Client(String name, String email) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.email = email;
+    }
 }
